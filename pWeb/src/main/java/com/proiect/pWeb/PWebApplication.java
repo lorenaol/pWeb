e085d1487218e851beba9220dd1a51c0720c32c3
Andreea-Lorena OLESCU (101496) <andreea.olescu@stud.acs.upb.ro>
2022-04-28 17:07:46 +0300

    first

---
+++ pWeb/src/main/java/com/proiect/pWeb/PWebApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.pWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PWebApplication.class, args);
+	}
+
+}
---
+++ pWeb/src/test/java/com/proiect/pWeb/PWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.pWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
